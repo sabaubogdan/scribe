1b3fabb9dd9de68933e6eead0fd9a7d67f3e554f
Bogdan Sabau <sabaubogdanvasile@gmail.com>
2018-01-19 22:00:09 +0200

    Added code to read pdf files. Changed the file reader to Map (map page number to page text(string))

--- src/main/java/xyz/vegaone/scribe/service/FileReaderServiceImpl.java
+++ src/main/java/xyz/vegaone/scribe/service/FileReaderServiceImpl.java
@@ -1,24 +1,64 @@
 package xyz.vegaone.scribe.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class FileReaderServiceImpl implements FileReaderService{
+@Slf4j
+@Service
+public class FileReaderServiceImpl implements FileReaderService {
 
-    public static void main(String[] args) {
-
-    }
     @Override
-    public List<String> contentReaderPdf() {
+    public Map<Integer, String> contentReaderPdf() throws IOException {
+
+        Map<Integer, String> pageNumberAndText = new HashMap<>();
+
+        List<String> textFromFile = new ArrayList<>();
+
+        FileDiscoveryServiceImpl fileDiscoveryService = new FileDiscoveryServiceImpl();
+        List<String> name = fileDiscoveryService.discoverFiles("C:\\Test").get("pdf");
+
+        for (int i = 0; i < name.size(); i++) {
+
+            File file = new File(name.get(i));
+
+            PDDocument document = PDDocument.load(file);
+            PDFTextStripper pdfTextStripper = new PDFTextStripper();
+
+            int firstPage = 1;
+
+            while (firstPage <= document.getNumberOfPages()) {
+
+                pdfTextStripper.setStartPage(firstPage);
+                pdfTextStripper.setEndPage(firstPage);
+                textFromFile.add(pdfTextStripper.getText(document));
+                pageNumberAndText.put(firstPage, textFromFile.toString());
+                firstPage++;
+
+            }
+            document.close();
+
+        }
+
+
+        return pageNumberAndText;
+    }
+
+    @Override
+    public Map<Integer, String> contentReaderExcel() {
         return null;
     }
 
     @Override
-    public List<String> contentReaderExcel() {
-        return null;
-    }
-
-    @Override
-    public List<String> contentReaderWord() {
+    public Map<Integer, String> contentReaderWord() {
         return null;
     }
 }
